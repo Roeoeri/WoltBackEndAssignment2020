1f1d9ce378c04ebad8326d5fa895f24a8f7f9901
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-02-09 23:09:08 +0200

    fixed the accidentaly removed which starts the server

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const http = require('http');
 const app = require('./app');
 
-http.createServer(app);
+const server = http.createServer(app);
+
+const PORT = 3001
+
+server.listen(PORT, () =>{
+
+})
+
+
